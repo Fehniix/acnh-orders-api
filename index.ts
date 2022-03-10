a7dc2c441862dd7ff74d693a33dca503ea0f6126
Johnny Bueti <29045904+Fehniix@users.noreply.github.com>
2022-03-10 22:11:38 +0100

    fix: added `SocketAPIEvent` to index export

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import { SocketAPIRequest } from "./lib/model/SocketAPIRequest";
 import { SocketAPIMessage } from "./lib/model/SocketAPIMessage";
 import { SocketAPIOptions } from "./lib/model/SocketAPIOptions";
+import { SocketAPIEvent } from "./lib/model/SocketAPIEvent";
 import SocketAPIClient from "./lib/SocketAPIClient";
 
-export { SocketAPIClient, SocketAPIRequest, SocketAPIMessage, SocketAPIOptions };
+export { SocketAPIClient, SocketAPIRequest, SocketAPIMessage, SocketAPIOptions, SocketAPIEvent };
